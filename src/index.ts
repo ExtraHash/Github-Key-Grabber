8302d6a3ad9aa7606349c3127ae4f8564cc1698b
ExtraHash <ExtraHash@protonmail.com>
2020-04-25 10:20:53 -0600

    run formatter'

--- src/index.ts
+++ src/index.ts
@@ -9,30 +9,32 @@ function getCLIArgs() {
   try {
     for (const arg of process.argv) {
       if (arg === '-u') {
-        username = process.argv[process.argv.indexOf('-u') + 1]
+        username = process.argv[process.argv.indexOf('-u') + 1];
       }
     }
-  } catch(error) {
+  } catch (error) {
     log.warn(error);
     process.exit(1);
   }
 
   if (!username) {
-    log.warn(`No username supplied. Syntax: ${chalk.yellow.bold('-u username')}`)
+    log.warn(
+      `No username supplied. Syntax: ${chalk.yellow.bold('-u username')}`
+    );
     process.exit(1);
   }
 
-  return [ username ];
+  return [username];
 }
 
 async function main() {
   loadEnvVars();
-  const [ userName ] = getCLIArgs()
-  const res = await ax.get(`https://api.github.com/users/${userName}/keys`)
-  for (const key of res.data.map((item: any) => (item.key))) {
-    fs.writeFileSync('authorized_keys', key + '\n', { flag: 'a+' })
+  const [userName] = getCLIArgs();
+  const res = await ax.get(`https://api.github.com/users/${userName}/keys`);
+  for (const key of res.data.map((item: any) => item.key)) {
+    fs.writeFileSync('authorized_keys', key + '\n', { flag: 'a+' });
   }
-  log.info('File written to authorized_keys in this project directory.')
+  log.info('File written to authorized_keys in this project directory.');
 }
 
 main();
